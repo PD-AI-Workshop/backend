5c745cda09cab229b1a2ae59930f664daf2a96d6
Alex Tyrdanov <alext0408@yandex.ru>
2024-09-04 18:04:23 +0300

    #1 feat: initialization Spring boot project

---
+++ src/test/java/com/aiworkshop/aiworkshop/AiworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiworkshop.aiworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
